7cad544e7a40b8b33a778f4713ac734f341d7f3f
Helder Ribeiro <helder@gmail.com>
2009-08-12 22:34:55 -0300

    Stops calling paid API method (map_rpx_identifier) by default.

--- lib/authlogic_rpx/acts_as_authentic.rb
+++ lib/authlogic_rpx/acts_as_authentic.rb
@@ -11,39 +11,39 @@ module AuthlogicRpx
         add_acts_as_authentic_module(Methods, :prepend)
       end
     end
-    
+
     module Methods
       # Set up some simple validations
       def self.included(klass)
         klass.class_eval do
           validates_uniqueness_of :rpx_identifier, :scope => validations_scope, :if => :using_rpx?
-          
+
           validates_length_of_password_field_options validates_length_of_password_field_options.merge(:if => :validate_password_with_rpx?)
           validates_confirmation_of_password_field_options validates_confirmation_of_password_field_options.merge(:if => :validate_password_with_rpx?)
           validates_length_of_password_confirmation_field_options validates_length_of_password_confirmation_field_options.merge(:if => :validate_password_with_rpx?)
-          
-          after_create :map_rpx_identifier
+
+          # after_create :map_rpx_identifier
         end
       end
-      
+
       private
-        
+
         def using_rpx?
           !rpx_identifier.blank?
         end
-        
+
         def validate_password_with_rpx?
           !using_rpx? && require_password?
         end
-        
+
         def map_rpx_identifier
           RPXNow.map(rpx_identifier, id) if using_rpx?
         end
-        
+
         def unmap_rpx_identifer
           RPXNow.unmap(rpx_identifier, id) if using_rpx?
         end
-        
+
     end
   end
 end
